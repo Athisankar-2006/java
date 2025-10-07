39dcb550233c0f850df05d3b71e22239754b0cd8
Athisankar-2006 <athisankar2k6@gmail.com>
2025-10-07 18:44:46 +0530

    five program added

---
+++ Basic and DataTypes/NameAge.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class NameAge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        System.out.println("Enter your Age:");
+        int age = sc.nextInt();
+        System.out.println("Hellow "+name+" you are "+age+" years old");
+
+        
+    }
+    
+}
+
+
