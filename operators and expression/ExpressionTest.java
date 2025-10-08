39b91c12b8571ecd25ecdd4f032a233a43b83aaf
Athisankar-2006 <athisankar2k6@gmail.com>
2025-10-08 17:22:02 +0530

    ten program added

---
+++ operators and expression/ExpressionTest.java
@@ -0,0 +1,10 @@
+public class ExpressionTest {
+    public static void main(String[] args) {
+        int a = 5, b = 10, c = 2;
+        int result = a + b * c - a / c; 
+        System.out.println("Result = " + result);
+    }
+}
+
+    
+
---
+++ operators and expression/ShiftOps.java
@@ -0,0 +1,7 @@
+public class ShiftOps {
+    public static void main(String[] args) {
+        int num = 8;
+        System.out.println("Left shift (num << 2): " + (num << 2));  // multiply by 4
+        System.out.println("Right shift (num >> 2): " + (num >> 2)); // divide by 4
+    }
+}
