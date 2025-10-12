558d8e6c181ae5b95539ebf9dc031d937593bc79
Athisankar-2006 <athisankar2k6@gmail.com>
2025-10-12 17:41:03 +0530

    guess number game was added

--- Control Flow Statements/GuessGame.java
+++ Control Flow Statements/GuessGame.java
@@ -1,21 +1,38 @@
 
-    import java.util.*;
+import java.util.Scanner;
 public class GuessGame {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int secret = 7;
-        while (true) {
-            System.out.print("Guess number (1-10): ");
-            int guess = sc.nextInt();
-            if (guess == secret) {
-                System.out.println("Correct! You win!");
-                break;
-            } else if (guess > secret)
-                System.out.println("Too high!");
-            else
-                System.out.println("Too low!");
+
+   public static void guessNumber() {
+    Scanner sc= new Scanner(System.in);
+    double number= 1+(int)(100*Math.random());
+    System.out.println(" guess the number between 1-100 :");
+    System.out.println("you have only five attemp to find !");
+    int k=5;
+    for (int i=0; i<k; i++){
+        System.out.print("Enter your "+(i+1)+"number :");
+        int guess =sc.nextInt();
+
+        if(number == guess){
+            System.out.println("you guessed the correct number at "+i+" attempt!..");
+            break;
         }
-        sc.close();
+        else if(number<guess){
+            System.out.println("it is too high than the answer ");
+        }
+        else{
+            System.out.println("it is too less than the answer");
+        }
+    }
+    System.out.println("You've exhausted all attempts. The correct number was: "+number);
+
+
+    sc.close();
+
+   }
+
+
+    public static void main(String[] args) {
+       guessNumber();
     }
 }
 
